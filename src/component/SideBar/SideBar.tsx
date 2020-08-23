a36ab0280f27a8a30b2a8d2620b3bfedb61d70de
senthilbalajiganesan <senthilbalaji.ganesan>
2020-08-23 20:33:30 +0530

    added logo in sidebar and checked working of svgs

--- src/component/SideBar/SideBar.tsx
+++ src/component/SideBar/SideBar.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { NotificationOutlined, SendOutlined } from "@ant-design/icons";
+import logo from "../../assets/svgs/tn-icon.svg";
 import { Layout, Menu } from "antd";
 import { MenuInfo } from "rc-menu/lib/interface";
 import { useHistory } from "react-router-dom";
@@ -9,14 +10,28 @@ import { useTranslation } from "react-i18next";
 const { Sider } = Layout;
 
 export default () => {
+  const [isSideBarCollapsed, setIsSideBarCollapsed] = useState<boolean>(false);
   const history = useHistory();
   const { t } = useTranslation();
 
   const onItemClick = (menuInfo: MenuInfo) =>
     history.push(menuInfo.key as string);
 
+  const onCollapse = () => setIsSideBarCollapsed(!isSideBarCollapsed);
+
   return (
-    <Sider collapsed={false} onCollapse={() => {}} collapsible width={250}>
+    <Sider
+      collapsed={isSideBarCollapsed}
+      onCollapse={onCollapse}
+      collapsible
+      className={styles.sideBar}
+    >
+      <div className={styles.logoContainer}>
+        <img src={logo} className={styles.logo} />
+        {!isSideBarCollapsed && (
+          <h1 className={styles.logoHeading}>Trash Nothing</h1>
+        )}
+      </div>
       <Menu className={styles.menuParent}>
         <Menu.Item
           onClick={onItemClick}
