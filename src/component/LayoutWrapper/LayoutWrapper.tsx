216e70be187edb120bb256380ee6ff6b91c3b973
senthilbalajiganesan <senthilbalaji.ganesan>
2020-08-23 18:41:45 +0530

    added layout wrapper style

--- src/component/LayoutWrapper/LayoutWrapper.tsx
+++ src/component/LayoutWrapper/LayoutWrapper.tsx
@@ -3,13 +3,12 @@ import { Layout } from "antd";
 import Header from "../Header/Header";
 import SideBar from "../SideBar/SideBar";
 import { ILayoutWrapper } from "./type";
-import "./layoutWrapperStyle.module.scss";
 
 const { Content } = Layout;
 
 export default ({ sider, header, children }: ILayoutWrapper) => {
   return (
-    <Layout className="layout-height">
+    <Layout style={{ minHeight: "100vh" }}>
       {sider ? sider : <SideBar />}
       <Layout>
         {header ? header : <Header />}
