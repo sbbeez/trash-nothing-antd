3db26a80cc3bc80b8c9e0223d2ae726d0bf7c8f9
senthilbalajiganesan <senthilbalaji.ganesan>
2020-08-26 00:34:30 +0530

    added image viewer for all images

--- src/component/PostmageList/PostmageList.tsx
+++ src/component/PostmageList/PostmageList.tsx
@@ -2,19 +2,50 @@ import React from "react";
 import ProgressiveImage from "../ProgressiveImage/ProgressiveImage";
 import { Photo } from "../../screen/Post/type";
 import { Empty } from "antd";
+import Viewer from "react-viewer";
 import styles from "./postImageListStyle.module.scss";
 
 export default (props: { photos: Photo[] | null }) => {
+  const [imageViewerDetails, setImageViewerDetails] = React.useState({
+    visible: false,
+    activeIndex: 0,
+  });
   if (!props.photos || props.photos.length === 0)
     return <Empty className={styles.alignSelfCenter} />;
   return (
-    <div className={styles.row}>
-      {(props.photos || []).map((photo) => (
-        <ProgressiveImage
-          imageUrl={photo.thumbnail}
-          blurHash={photo.blurhash}
-        />
-      ))}
-    </div>
+    <>
+      <div className={styles.row}>
+        {(props.photos || []).map((photo, index) => (
+          <ProgressiveImage
+            imageUrl={photo.thumbnail}
+            blurHash={photo.blurhash}
+            width={
+              (photo.images && photo.images[0] && photo.images[0].width) || 0
+            }
+            height={
+              (photo.images && photo.images[0] && photo.images[0].height) || 0
+            }
+            index={index}
+            onClick={(selectedIndex: number) => {
+              setImageViewerDetails({
+                visible: true,
+                activeIndex: selectedIndex,
+              });
+            }}
+          />
+        ))}
+      </div>
+      <Viewer
+        visible={imageViewerDetails.visible}
+        activeIndex={imageViewerDetails.activeIndex}
+        onClose={() => {
+          setImageViewerDetails({
+            visible: false,
+            activeIndex: 0,
+          });
+        }}
+        images={(props.photos || []).map((photo) => ({ src: photo.url }))}
+      />
+    </>
   );
 };
--- src/component/ProgressiveImage/ProgressiveImage.tsx
+++ src/component/ProgressiveImage/ProgressiveImage.tsx
@@ -1,27 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Blurhash } from "react-blurhash";
+import styles from "./progressiveImage.module.scss";
 
-export default (props: { blurHash: string; imageUrl: string }) => {
+export default ({
+  blurHash,
+  imageUrl,
+  width,
+  height,
+  index,
+  onClick,
+}: {
+  blurHash: string;
+  imageUrl: string;
+  width: number;
+  height: number;
+  index: number;
+  onClick: (selectedIndex: number) => void;
+}) => {
   const [imageLoadState, setImageLoadState] = useState("loading");
   const onLoadSuccess = () => setImageLoadState("success");
   const onLoadError = () => setImageLoadState("error");
   return (
-    <>
+    <div className={styles.imageContent}>
       <Blurhash
-        hash={props.blurHash}
-        width={imageLoadState === "loading" ? 300 : 0}
-        height={imageLoadState === "loading" ? 400 : 0}
+        hash={blurHash}
+        width={imageLoadState === "loading" ? width : 0}
+        height={imageLoadState === "loading" ? height : 0}
         resolutionX={32}
         resolutionY={32}
         punch={0}
       />
       <img
-        width={imageLoadState === "success" ? 300 : 0}
-        height={imageLoadState === "success" ? 400 : 0}
-        src={props.imageUrl}
+        width={imageLoadState === "success" ? width : 0}
+        height={imageLoadState === "success" ? height : 0}
+        src={imageUrl}
         onLoad={onLoadSuccess}
         onError={onLoadError}
+        onClick={() => onClick(index)}
       />
-    </>
+    </div>
   );
 };
